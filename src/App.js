a06eb81ad23c85b03fba9fd7d3bc084b8e19fef1
lauren-richardson <lauren.richardson13@gmail.com>
2022-03-30 15:06:28 -0400

    small bug fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Components/Header/Header'
-import './index.css'
+
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <h1>Hello! My name is </h1>
       <h2>Lauren L. Richardson</h2>
       <h1>I'm a software engineer.</h1>
-      
+      <img src='https://github.com/l-Rich/lauren-r-portfolio/blob/master/public/Untitled%20design%20(2).png?raw=true' />
     </div>
   )
 }

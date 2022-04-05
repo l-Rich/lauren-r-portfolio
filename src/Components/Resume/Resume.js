e836135f7bf48cb114e9d5583acdd6ce1d074e5e
lauren-richardson <lauren.richardson13@gmail.com>
2022-04-05 14:33:38 -0400

    resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -4,7 +4,8 @@ import './Resume.css'
 const Resume = () => {
   return (
     <div className="resume">
-        <h1 className="resume_header">Resume</h1>
+        <div className='resume1'><h1 className='resume_header_resume'>What I've Done</h1><img className='resume_work_image' src='https://github.com/l-Rich/lauren-r-portfolio/blob/master/public/Resume_1.png?raw=true'/></div>
+        <div className='resume1'><h1 className='resume_header_school'>Where I've Been</h1><img className='resume_work_image' src='https://github.com/l-Rich/lauren-r-portfolio/blob/master/public/School.png?raw=true'/></div>    
     </div>
   )
 }
